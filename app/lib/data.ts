286e2c7a1212af1574ebf6183476c715595850a8
Tian <xtyao2015@gmail.com>
2024-04-02 20:45:19 -0400

    added header animation

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import nba from "@/public/nba.jpg";
+import book from "@/public/book.jpeg";
+import med from "@/public/med.jpg";
+import netflix from "@/public/netflix.jpg"
 
 export const links = [
   {
@@ -62,25 +63,32 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "NBA News and Statistics",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    tags: ["React", "Next.js", "MongoDB", "CSS", "Prisma"],
+    imageUrl: nba,
   },
   {
-    title: "rmtDev",
+    title: "RAG Book Recommender",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    tags: ["Python", "LangChain", "Hugging Face", "Streamlit"],
+    imageUrl: book,
   },
   {
-    title: "Word Analytics",
+    title: "Doctor-Patient Dashboard",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: med,
+  },
+  {
+    title: "Video Streaming Studio",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: netflix,
   },
 ] as const;
 
